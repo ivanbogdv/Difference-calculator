ffc619f0cab8b61bba676a97c6f5a5a22c5fb938
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2023-02-27 22:26:57 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 #!/usr/src/env node
 import { readFileSync } from "fs";
+import path from "path";
+import process from "process";
 import genDiff from "./genDiff.js";
 import _ from "lodash";
 
 
 export default (filepath1, filepath2) => {
+
   
-  const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8');
+
+  const data1 = process.cwd(readFileSync(filepath1, 'utf-8'));
+  const data2 = process.cwd(readFileSync(filepath2, 'utf-8'));
+
+
 
   const ParseData1 = JSON.parse(data1);
   const ParseData2 = JSON.parse(data2);
 
-  // console.log(ParseData1);
-  // console.log(ParseData2);
   
-  console.log( genDiff(ParseData1, ParseData2));
+  // console.log(genDiff(ParseData1, ParseData2));
   genDiff(ParseData1, ParseData2);
-  // console.log(ParseData1);
-  // console.log(ParseData2);
+
 
 };
