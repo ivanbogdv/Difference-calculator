ea36c1191ebb84ca406f3639097689c524b3f20b
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2023-04-01 15:35:57 +0300

    Fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,35 @@
 const makeStylish = (tree) => {
-  console.log(tree);
-  const result = tree.map((item) => {
-    const matchesKey = `  ${item.key}`;
-    const plusKey = `+ ${item.key}`;
-    const minusKey = `- ${item.key}`;
-
-    switch (item.type) {
-      case 'added':
-        return `${plusKey}: ${item.value} `;
-      case 'unchanged':
-        return `${matchesKey}: ${item.value} `;
-      case 'changed':
-        return `${minusKey}: ${item.value[0]}\n${plusKey}: ${item.value[1]}`;
-      case 'deleted':
-        return `${minusKey}: ${item.value} `;
-      default:
-        break;
+  const toString = (node) => {
+    if (!Array.isArray(node)) {
+      return node;
     }
-  });
-  return result.join('\n');
+
+    const result = tree.map((item) => {
+      const matchesKey = `  ${item.key}`;
+      const plusKey = `+ ${item.key}`;
+      const minusKey = `- ${item.key}`;
+
+      switch (item.type) {
+        case 'added':
+          return `${plusKey}: ${item.value} `;
+        case 'deleted':
+          return `${minusKey}: ${item.value} `;
+        case 'unchanged':
+          return `${matchesKey}: ${item.value} `;
+        case 'nested': {
+          const children = item.children.map((child) => toString(child));
+          return ` ${item.key}: ${toString(children)}`;
+        }
+        case 'changed': {
+          return `${minusKey}: ${item.value[0]}\n${plusKey}: ${item.value[1]}`;
+        }
+        default:
+          throw new Error(`Error: ${item} - this type doesn't exist in this file`);
+      }
+    });
+    return result;
+  };
+  return toString(tree);
 };
 
 export default makeStylish;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 #!/usr/bin/env node
 
 import { readFileSync } from 'fs';
-// import _ from 'lodash';
 import path from 'path';
 import { cwd } from 'process';
 import parser from './parsers.js';
 import buildAST from './buildAST.js';
 import formatter from './formatters/indexFormatter.js';
-// import genDiffOld from './genDiffOld.js';
-
-const generateDiff = (obj1, obj2, format) => {
-  const ast = buildAST(obj1, obj2);
-  return formatter(ast, format);
-};
 
 const getPath = (filepath) => path.resolve(cwd(), filepath);
 const getFileFormat = (filepath) => path.extname(filepath).slice(1);
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2, format) => {
   const path1 = getPath(filepath1);
   const path2 = getPath(filepath2);
 
-  const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8');
+  const obj1 = readFileSync(filepath1, 'utf-8');
+  const obj2 = readFileSync(filepath2, 'utf-8');
 
-  const parseData1 = parser(data1, getFileFormat(path1));
-  const parseData2 = parser(data2, getFileFormat(path2));
+  const parseObj1 = parser(obj1, getFileFormat(path1));
+  const parseObj2 = parser(obj2, getFileFormat(path2));
 
-  const result = generateDiff(parseData1, parseData2, format);
-  return result;
+  const result = buildAST(parseObj1, parseObj2);
+  return formatter(result, format);
 };
 
 export default genDiff;
