5dff9f5f768873f5a9c6fe3270b2690107c582d9
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2023-04-04 14:52:59 +0300

    Fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,6 +4,12 @@ const indent = ' ';
 const indentSize = 4;
 const currentIndent = (depth) => indent.repeat(indentSize * depth - 2);
 const braceIndent = (depth) => indent.repeat(indentSize * depth - indentSize);
+// new
+const replacer = ' ';
+const doubleSpace = '  ';
+const spacesCount = 4;
+//
+const getIndent = (depth) => replacer.repeat(depth * spacesCount).slice(0, -2);
 
 const joinString = (lines, depth) => [
   '{',
@@ -12,7 +18,7 @@ const joinString = (lines, depth) => [
 ].join('\n)');
 
 const stringify = (data, depth) => {
-  if ((!_.isObject(data)) || (data === null)) {
+  if ((!_.isPlainObject(data)) || (data === null)) {
     return String(data);
   }
   const keys = _.keys(data);
@@ -29,9 +35,10 @@ const makeStylishDiff = (tree) => {
       }
       case 'nested': {
         // const children = node.children || [];
-        console.log('-------------------------', node.children);
-        const childrenToString = node.children.flatMap((child) => iter(child, depth + 1));
-        return `${currentIndent(depth)}  ${node.key}: ${joinString(childrenToString, depth + 1)}`;
+        // console.log('-------------------------', node.children);
+        // const childrenToString = children.flatMap((child) => iter(child, depth + 1));
+      // return `${currentIndent(depth)}  ${node.key}: ${joinString(childrenToString, depth + 1)}`;
+        return `${getIndent(depth)}  ${node.key}: {\n${iter(node.value, depth + 1).join('\n')}\n${getIndent(depth)}${doubleSpace}}`;
       }
       case 'added': {
         return `${currentIndent(depth)}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
