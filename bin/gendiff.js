f00520773c6e9f5b8a9757b0b8b7ad280ec8fd6a
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2023-04-08 13:32:55 +0300

    Fix error

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 import genDiff from '../src/index.js';
-
-const program = new Command();
 
 program
   .version('0.0.1')
